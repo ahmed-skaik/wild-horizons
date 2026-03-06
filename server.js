5831dd8d4c898c445e7e065cb50de40e21e9b407
Ahmed Skaîk <ahmedskaik.2001@gmail.com>
2026-03-06 17:01:27 +0200

    creating my first pseudo-server

--- server.js
+++ server.js
@@ -1,1 +1,9 @@
-console.log("Hello from firsr Node.jS project ");
+import http from "node:http";
+
+const PORT = 8000;
+
+const server = http.createServer((req, res) => {
+  res.end("Hello from Node JS server");
+});
+
+server.listen(PORT, () => console.log(`This Server runs on port : ${PORT}`));
